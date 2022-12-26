5ea8d08e103228f6837bf9b789ad3436c31e11b8
Alfonso JL <ajavierlombardi@gmail.com>
2022-12-26 09:52:14 -0300

    little step foward

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/home.html'))
+    res.sendFile(path.join(__dirname,'/views/index.html'))
 });
 app.listen(3030, () => {
     console.log('Servidor iniciado en http://localhost:3030');
