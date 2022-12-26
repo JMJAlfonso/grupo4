72d98493c4676d1000ea0b3c78ec3ac35afc04ef
Alfonso JL <ajavierlombardi@gmail.com>
2022-12-26 11:19:04 -0300

    resolving app.js git  error

--- app.js
+++ app.js
@@ -4,20 +4,8 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-<<<<<<< HEAD
-<<<<<<< HEAD
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
-=======
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
->>>>>>> ed313bb3da3fd1c1fbd7289871d348911495b72c
-=======
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'));
->>>>>>> ed313bb3da3fd1c1fbd7289871d348911495b72c
 });
 
 app.listen(3030, () => {
