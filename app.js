9666d44dbf89a93d153a5baeb896c30d6df5b87a
Bruno Fabián Cabral <minhyllest@gmail.com>
2022-12-29 20:48:03 -0300

    ?

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 });
-<<<<<<< HEAD
 app.use(express.static('public'));
-app.get('/',(req,res)=>{
+app.get('/productDetail',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/productDetail.html'))
 });
-=======
 
 app.use(express.static('public'));
 app.get('/productCart',(req,res)=>{
@@ -24,7 +22,6 @@ app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'))
 });
 
->>>>>>> a2a78e60cc6f802ab451f008cd38547a71ef5c87
 app.listen(3030, () => {
     console.log('Servidor iniciado en http://localhost:3030');
 });
