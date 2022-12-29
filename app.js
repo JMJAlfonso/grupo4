09b8451b26851c7f77cbbf4f5e68d56cd4063c58
mickyliddell <yandrade131@gmail.com>
2022-12-29 20:29:28 -0300

    terminado1

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 });
-
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/productDetail.html'))
+});
 app.listen(3030, () => {
     console.log('Servidor iniciado en http://localhost:3030');
 });
