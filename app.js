0153aba135c028fc8e3d39f5d3fdaf971a343efe
Alfonso JL <ajavierlombardi@gmail.com>
2022-12-29 21:26:13 -0300

    solution to app.js problems

--- app.js
+++ app.js
@@ -7,24 +7,23 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/index.html'))
 });
-<<<<<<< HEAD
-app.use(express.static('public'));
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'/views/productDetail.html'))
-});
-=======
 
-app.use(express.static('public'));
+
 app.get('/productCart',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/productCart.html'))
 });
 
-app.use(express.static('public'));
+
 app.get('/register',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/register.html'))
 });
 
->>>>>>> a2a78e60cc6f802ab451f008cd38547a71ef5c87
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+
+
 app.listen(3030, () => {
     console.log('Servidor iniciado en http://localhost:3030');
 });
+
