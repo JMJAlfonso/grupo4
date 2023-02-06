bc409b859368a9609838de172974d3fb5c1bd930
Alfonso JL <ajavierlombardi@gmail.com>
2023-02-06 10:58:37 -0300

    changings made for script4

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,16 +1,38 @@
 const fs = require("fs");
 const path = require('path');
 
-const productsJson = fs.readFileSync(path.join(__dirname, '../../data/products.json'), "utf-8");
-
-const products = JSON.parse(productsJson);
-
+const productFilePath = path.join(__dirname, '../../data/products.json');
+const productsJson = fs.readFileSync(productFilePath, "utf-8");
+let products = JSON.parse(productsJson);
 
 const productController = {
+
     createProduct: function (req, res){
         //console.log(products);
 
        res.render('createProduct'); 
+    },
+    editProduct: function (req, res){
+        const idProductEdit = req.params.id;
+        let productEdit = products.find((product)=> product.id ==idProductEdit);
+        console.log(productEdit);
+       res.render('editProduct',{product:productEdit}); 
+    },
+    saveEditProduct: function(req,res){
+        let productEdited = req.body;        
+        products.forEach(function(product){
+            if(product.id == productEdited.id){
+                product.name = productEdited.name;
+                product.description = productEdited.description;
+            }
+                
+                    
+                
+        });
+                        
+        let productsJson = JSON.stringify(products);
+        fs.writeFileSync(productFilePath,productsJson);
+        res.redirect('/');
     }
 }
 
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 //list es el nombre de la lista de productos
-router.get('/create', productController.createProduct);
+router.get('/createProduct', productController.createProduct);
+
+router.get('/:id/edit',productController.editProduct);
+router.put('/edit',productController.saveEditProduct)
 
 module.exports = router;
 
