b315568d929789f719050b5354a4f76888a7bc6a
Alfonso JL <ajavierlombardi@gmail.com>
2023-03-07 17:37:35 -0300

    cambio nombre al authmiddle por userMiddlewares

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const path = require('path');
 const multer = require('multer');
 const guestMiddleware = require('../middlewares/guestMiddlewares');
-const authMiddleware = require('../middlewares/authMiddlewares');
+const userMiddleware = require('../middlewares/userMiddlewares');
 const validation = require('../validation/userValidation');
 
 //configuracion de multer para almacenar imaganes
@@ -24,7 +24,7 @@ const upload = multer({ storage });
 router.get('/register', guestMiddleware, usersController.register);
 router.post('/register', upload.single('userImage'), validation.register, usersController.registerProcess);
 router.get("/login", guestMiddleware,usersController.login);
-router.get('/productCart',authMiddleware, usersController.productCart);
+router.get('/productCart',userMiddleware, usersController.productCart);
 router.post("/login",validation.login, usersController.loginProcess);
 
 router.get('/check', function (req, res) {
