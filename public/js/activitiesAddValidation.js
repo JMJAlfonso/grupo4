419224f82aecf767ab00b518c1bcbb96b77b0f29
Alfonso JL <ajavierlombardi@gmail.com>
2023-04-18 00:39:50 -0300

    validation creat products front end DONE

--- public/js/activitiesAddValidation.js
+++ public/js/activitiesAddValidation.js
@@ -8,19 +8,22 @@ window.onload = function () {
         e.preventDefault();
         const allErrorLabels = document.querySelectorAll('.show-error-message');
         allErrorLabels.forEach(element =>{
+            element.classList.remove('show-error-message');
             element.innerHTML = '';
         })
         const errors = [];
 
         if (!form.name.value) {
             errors.push({ name: 'name', message: 'El campo nombre no puede estar vacio' });
-
+            form.name.classList.add('is-invalid');
         }
         if (form.name.value.length < 5 || form.name.value === '') {           
             errors.push({ name: 'name', message: 'El campo nombre debe tener al menos 5 caracteres' });
+            form.name.classList.add('is-invalid');
         }
-        if (form.description.value.length < 20) {
+        if (form.description.value.trim().length < 20) {
             errors.push({ name: 'description', message: 'El campo descripcion debe tener al menos 20 caracteres' });
+            form.description.classList.add('is-invalid');
         }
 
         const path = form.image.value;
@@ -30,8 +33,9 @@ window.onload = function () {
         const splitName = path.split('.');
         const extention = splitName[splitName.length - 1];
 
-        if (!allowedExtensions.include(extention)) {
+        if (!allowedExtensions.includes(extention)) {
             errors.push({ name: 'image', message: 'El formato de imagen debe ser .jpg , .jpeg, .png , .gif ' });
+            form.image.classList.add('is-invalid');
         }
         errors.forEach(error => {
             const errorLabel = document.getElementById(`error-${error.name}`)
