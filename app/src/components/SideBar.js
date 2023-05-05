727e919ae79cfd136ce56d8582aa0a585dd27057
fabianloza <fabianloza.fgl@gmail.com>
2023-05-05 15:10:31 -0300

    Arreglado los README.md que habian desaparecido

--- app/src/components/SideBar.js
+++ app/src/components/SideBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import image from '../assets/images/logo-DH.PNG';
+import image from '../assets/images/logo-DH.png';
 
 function SideBar() {
     return (
